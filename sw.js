e78ccaf8cf01007cf5dde8f2b0a257dc273a9cfe
Ivan25G <ivyyo@hotmail.com>
2026-08-19 14:01:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'turnos-cache-v1';
+const CACHE_NAME = 'turnos-cache-v2';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
